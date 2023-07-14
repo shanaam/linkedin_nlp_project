f58299c4f310275a4cf0ffae6b271c6e9972f46d
shanaa <s.modcha@gmail.com>
2023-07-13 22:27:54 -0400

    added tokenizing dataset class

--- embeddings/embeddings_sm.py
+++ embeddings/embeddings_sm.py
@@ -1,3 +1,48 @@
 """
 Embedding models for a dataset of text.
 """
+from torch.utils.data import Dataset
+
+
+def embed_sm(dataset):
+    """
+    Embeds the text in a dataset using a simple embedding model.
+    
+    Parameters:
+        dataset (dataset): A dataset of strings and labels. Each string is a sentence or phrase.
+    """
+    result = []
+
+    return result
+
+
+# class to tokenize the dataset
+class TokenizedC4Dataset(Dataset):
+    """
+    A dataset that tokenizes the text in a dataset.
+    """
+
+    def __init__(self, dataset, encoding):
+        self.dataset = dataset
+        self.encoding = encoding
+
+    def __getitem__(self, idx):
+        text = self.dataset[idx]["text"]
+        label = self.dataset[idx]["label"]
+
+        text = self.encoding.encode(text)
+        return text, label
+
+    def preprocess_toktext_custom(self, text):
+        """
+        Preprocess the tokenized text for non-rnn models.
+        """
+        # if the list is less than 30, pad it with 0s
+        if len(text) < 30:
+            text = [0] * (30 - len(text)) + text
+
+        # if the list is greater than 30, truncate it
+        if len(text) > 30:
+            text = text[:30]
+
+        return text
